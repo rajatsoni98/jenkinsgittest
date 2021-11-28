8e8a6dabf3a1ab013aec0e48fd9812e5e082b0d6
Rajat Soni <rajat.soni98@gmail.com>
2021-11-28 18:04:26 +0530

    modified intrest

--- intrest.py
+++ intrest.py
@@ -1,5 +1,7 @@
-p=int(input("Enter principal amount: "))
-r=int(input("Enter intrest rate: "))
-t=int(input("Enter time: "))
-si=(p*r*t)/100
-print("The simple intrest is: ",si)
+def pat(n):
+    for i in range(1,n+1):
+        print(("* ")*(n+1-i)+(" "*(i-1)))
+
+pat(3)
+pat(4)
+pat(8)
